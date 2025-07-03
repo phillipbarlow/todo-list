9bded36992f470f71639eadaac3a86f5bc266ca2
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2025-07-03 19:51:24 +0100

    updated basic styling

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,18 +1,18 @@
 export default function AddTodoForm({onAddTodo, input, setInput}){
-    
     const newTodo = {
-      id:Date.now(),
-      text:input,
-      completed:false
+        id:Date.now(),
+        text:input,
+        completed:false
     }
     function handleSubmit(e){
+        if (!input) return
         e.preventDefault()
         onAddTodo(newTodo)
         setInput("")
     }
     return(
         <form className="addTodoForm" onSubmit={handleSubmit}>
-            <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
+            <input type="text" value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Enter todo here!"/>
             <button>+</button>
         </form>
     )
